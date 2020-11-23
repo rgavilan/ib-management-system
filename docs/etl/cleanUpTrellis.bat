cb4b48b874cfed4fc86a2e22bc0cdd1476c68f1f
Alejandro Martino Uria <amartino@izertis.com>
2020-11-23 15:28:41 +0100

    SEMANTMURC-381 tutorial

---
+++ docs/etl/cleanUpTrellis.bat
@@ -0,0 +1,26 @@
+set managementSystemPath=C:\Users\amartino\Documents\murcia\management-system\docker-devenv
+set /P managementSystemPath="management-system path? default [%managementSystemPath%]"
+
+set tripleStoragePath=C:\Users\amartino\Documents\murcia\triples-storage-adapter\docker-devenv\fuseki-trellis
+set /P tripleStoragePath="triplestorage path? default [%tripleStoragePath%]"
+
+docker stop asio_kafka_1
+docker stop asio_zookeeper_1
+docker stop fuseki-sandbox
+docker stop trellis
+
+docker container prune -f
+docker volume prune -f
+docker network prune -f
+
+cd %managementSystemPath% 
+docker-compose up -d
+cd %tripleStoragePath%
+docker-compose up -d
+
+cd c:\desarrollo\bat-files
+
+
+
+
+
