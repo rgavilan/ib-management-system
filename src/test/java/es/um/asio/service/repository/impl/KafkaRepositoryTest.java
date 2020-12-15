95b9cf906beec4d2aa1979dd75d1a2efaeeacf5b
pteran <pteran@izertis.com>
2020-12-15 08:43:25 +0100

    [SEMANTMURC-462] Test java Added test

---
+++ src/test/java/es/um/asio/service/repository/impl/KafkaRepositoryTest.java
@@ -0,0 +1,56 @@
+package es.um.asio.service.repository.impl;
+
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import es.um.asio.abstractions.domain.ManagementBusEvent;
+import es.um.asio.service.repository.KafkaRepository;
+
+
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("unit-test")
+public class KafkaRepositoryTest {
+	
+	@Autowired
+	private KafkaRepository kafkaRepository;
+	
+	@MockBean
+    private KafkaTemplate<String, ManagementBusEvent> kafkaTemplate;
+	
+	private String managementTopicName = "management-data";
+	
+	@TestConfiguration
+	static class KafkaRepositoryImplTestConfig {
+		@Bean
+		KafkaRepository kafkaRepository() {
+			return new KafkaRepositoryImpl();
+		}
+	}
+	
+	@Before
+    public void setUp() {
+        ReflectionTestUtils.setField(kafkaRepository, "managementTopicName", managementTopicName);
+    }
+	
+	@Test
+	public void send() {
+		ManagementBusEvent message = new ManagementBusEvent();
+		kafkaRepository.send(message);
+		verify(kafkaTemplate, times(1)).send(managementTopicName, message);
+	}
+
+}
