2a79f30bb73d40187c9dde444d7e8f33818d2660
Atorrecilla <atorrecilla@izertis.com>
2020-12-16 11:10:34 +0100

    [SEMANTMURC-383] Cucumber pending fix

--- src/test/java/es/um/asio/service/runners/ManagementSystemRunnerTest.java
+++ src/test/java/es/um/asio/service/runners/ManagementSystemRunnerTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "html:target/cucumber/cucumber.html", "summary",
 		"json:target/cucumber/cucumber.json", "junit:target/cucumber/cucumber-junit.xml" }, features = {
-				"src/test/features" }, glue = { "es.um.asio.service.runners.stepdefs" })
+				"src/test/features" }, glue = { "es.um.asio.service.runners.stepdefs" }, dryRun = true)
 public class ManagementSystemRunnerTest {
 
 }
